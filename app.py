897b9143a2139d69f08d30181d07df6314c8705d
Mario Molina <71576056+MarioMolina85@users.noreply.github.com>
2026-06-22 20:52:15 -0500

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,6 @@ def obtener_empresas():
         "Bitcoin ETF": "BITO",
         "Petrolio":"CL=F"
     }
-@st.cache_data.clear()
 #funcion para cargar la data 
 @st.cache_data
 def descargar_datos(ticker, fecha_inicio, fecha_fin):
